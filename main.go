29d38cca19fe20c71054fc6e646e7f68751f9a78
Rhys Davies <rhys@Rhyss-MacBook-Air.local>
2024-07-25 09:54:51 -0700

    Init commit

--- main.go
+++ main.go
@@ -5,49 +5,65 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"time"
 )
 
-var (
-	templates = map[string]TemplateConfig{}
-)
+var box *rice.Box
+var counter = 1
 
-type TemplateConfig struct {
-	Name       string
-	Path       string
-	Content    string
-	HandleFunc func(w http.ResponseWriter, r *http.Request)
+func createRow(w http.ResponseWriter, _ *http.Request) {
+	counter++
+	log.Printf("Counter incremented to: %d", counter)
+	ret := `
+		<tr>
+			<td>Rhys Davies</td>
+			<td>52223423</td>
+			<td><button hx-delete="/rows" hx-target="closest tr" hx-swap="outerHTML">Remove</button></td>
+		</tr>
+	`
+	_, _ = w.Write([]byte(ret))
+
+}
+
+func deleteRow(w http.ResponseWriter, _ *http.Request) {
+	counter--
+	log.Printf("Counter decremented to: %d", counter)
+}
+
+func getAll(w http.ResponseWriter, _ *http.Request) {
+	var ret string
+	for i := 0; i < counter; i++ {
+		ret += `
+			<tr>
+				<td>Rhys Davies</td>
+				<td>52223423</td>
+				<td><button hx-delete="/rows" hx-target="closest tr" hx-swap="outerHTML">Remove</button></td>
+			</tr>
+		`
+	}
+	log.Printf("Returning: %s", ret)
+	_, _ = w.Write([]byte(ret))
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
-	t := templates["index"]
-	msg, err := template.New(t.Name).Parse(t.Content)
+	content := box.MustString("index.html")
+	tmpl := template.Must(template.New("index").Parse(content))
+	data := map[string]string{
+		"Title":       "Big Test Page",
+		"Description": "Rhys Testing out HTMX Golang",
+	}
+	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
-	}
-	err = msg.Execute(w, map[string]string{"Message": time.Now().String()})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func initTemplates() {
-	box := rice.MustFindBox("templates")
-	templates = map[string]TemplateConfig{
-		"index": {
-			Path:       "GET /",
-			HandleFunc: index,
-			Content:    box.MustString("index.html"),
-		},
-	}
-	for k, v := range templates {
-		http.HandleFunc(v.Path, v.HandleFunc)
-		log.Printf("Loaded template: %v\n", k)
 	}
 }
 
 func main() {
-	initTemplates()
+	box = rice.MustFindBox("templates")
+	http.HandleFunc("GET /", index)
+	http.HandleFunc("GET /rows", getAll)
+	http.HandleFunc("POST /rows", createRow)
+	http.HandleFunc("DELETE /rows", deleteRow)
+
 	log.Printf("Listening on: %s", "localhost:8080")
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
