d80e3e5789a9c6c099f5f203368ad6d3ca43f715
BillyUdders <rhys.davies901@gmail.com>
2024-07-26 19:35:15 -0700

    Working delete

--- main.go
+++ main.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/jmoiron/sqlx"
 	"html/template"
 	"log"
 	"net/http"
 )
-
-var createTableStmt = `
-	CREATE TABLE IF NOT EXISTS rows (
-		id  integer primary key,
-		name text,
-		age integer
-	)
-`
 
 type Row struct {
 	ID   int64  `db:"id"`
@@ -32,14 +25,33 @@ var (
 		"Title":       "Big Test Page",
 		"Description": "Holden x Bill: Aligulac",
 	}
-	rowTemplate = template.Must(template.New("row-partial").Parse(`
-		<tr>
-			<td>{{.Name}}</td>
-			<td>{{.Age}}</td>
-			<td><button hx-delete="/rows" hx-target="closest tr" hx-swap="outerHTML">Remove</button></td>
-		</tr>
-	`))
+	createTableStmt = `
+		CREATE TABLE IF NOT EXISTS rows (
+			id  integer primary key,
+			name text,
+			age integer
+		)
+	`
 )
+
+func Render(w http.ResponseWriter, row Row) error {
+	html := `
+		<tr>
+			<td id="id">{{.ID}}</td>
+			<td id="name">{{.Name}}</td>
+			<td id="age">{{.Age}}</td>
+			<td id="delete-button"><button hx-delete="/rows/{{.ID}}" hx-target="closest tr" hx-swap="outerHTML">Remove</button></td>
+		</tr>
+	`
+	return template.Must(template.New(fmt.Sprintf("%v-row", row.ID)).Parse(html)).Execute(w, row)
+}
+
+func getRow(w http.ResponseWriter, _ *http.Request) {
+	err := Render(w, Row{})
+	if err != nil {
+		return
+	}
+}
 
 func getRows(w http.ResponseWriter, _ *http.Request) {
 	rows, err := getAll[Row](db, "SELECT * FROM rows")
@@ -47,7 +59,8 @@ func getRows(w http.ResponseWriter, _ *http.Request) {
 		handleError(w, err, http.StatusInternalServerError)
 	}
 	for i := range rows {
-		err = rowTemplate.Execute(w, rows[i])
+		row := rows[i]
+		err = Render(w, row)
 		if err != nil {
 			return
 		}
@@ -55,10 +68,9 @@ func getRows(w http.ResponseWriter, _ *http.Request) {
 }
 
 func createRow(w http.ResponseWriter, r *http.Request) {
-	var row Row
-	err := json.NewDecoder(r.Body).Decode(&row)
+	row, err := parseBody[Row](r)
 	if err != nil {
-		handleError(w, err, http.StatusInternalServerError)
+		handleError(w, err, http.StatusBadRequest)
 		return
 	}
 	insertId, err := insert(db, "INSERT INTO rows (name, age) VALUES (?, ?)", row.Name, row.Age)
@@ -67,31 +79,30 @@ func createRow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	row.ID = insertId
-	err = rowTemplate.Execute(w, row)
+	err = Render(w, row)
 	if err != nil {
 		handleError(w, err, http.StatusInternalServerError)
 		return
 	}
 }
 
-func handleError(w http.ResponseWriter, err error, errCode int) {
-	errLog.Println(err.Error())
-	http.Error(w, err.Error(), errCode)
-}
-
-func getRow(w http.ResponseWriter, _ *http.Request) {
-	err := rowTemplate.Execute(w, nil)
+func deleteRow(w http.ResponseWriter, r *http.Request) {
+	_, err := db.Exec("DELETE FROM rows WHERE id = ?", r.PathValue("id"))
 	if err != nil {
+		handleError(w, err, http.StatusInternalServerError)
 		return
 	}
 }
 
-func deleteRow(w http.ResponseWriter, _ *http.Request) {
-	//handleError(w, errors.New("unimplemented"), 500)
-	//_, err := w.Write([]byte("Holdennello"))
-	//if err != nil {
-	//	return
-	//}
+func parseBody[T any](r *http.Request) (T, error) {
+	var t T
+	err := json.NewDecoder(r.Body).Decode(&t)
+	return t, err
+}
+
+func handleError(w http.ResponseWriter, err error, errCode int) {
+	errLog.Println(err.Error())
+	http.Error(w, err.Error(), errCode)
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
@@ -115,9 +126,9 @@ func main() {
 
 	http.HandleFunc("GET /", index)
 	http.HandleFunc("GET /rows", getRows)
-	http.HandleFunc("GET /rows/{id}", getRow)
 	http.HandleFunc("POST /rows", createRow)
-	http.HandleFunc("DELETE /rows", deleteRow)
+	http.HandleFunc("GET /rows/{id}", getRow)
+	http.HandleFunc("DELETE /rows/{id}", deleteRow)
 
 	log.Printf("Listening on: %s", addr)
 	err = http.ListenAndServe(addr, nil)
